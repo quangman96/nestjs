3c45515341d4da5b3fa58cde78261d014a1a0385
mannq <mannq@aureole-it.vn>
2020-12-22 10:50:25 +0700

    Initialize sourcebase

---
+++ src/commons/consts.ts
@@ -0,0 +1,3 @@
+export declare const SYSTEM_CLASS = "000000000000000000000000000000000000";
+export declare const STRING_EMPTY = "";
+export declare const STRING_SPACE = " ";
---
+++ src/commons/enums.ts
@@ -0,0 +1,56 @@
+export declare enum KEYS {
+    USER_ID = "user_id",
+    COMPANY = "company",
+    DEPT_ID = "dept_id",
+    EMP_ID = "emp_id",
+    LANG = "lang",
+    MODULE = "module",
+    PAGE = "page",
+    ID = "id",
+    CODE = "code",
+    NAME = "name",
+    ACTIVE_FLAG = "active_flag",
+    SORT_NO = "sort_no",
+    CHANGE_COUNT = "change_count",
+    CREATE_BY = "create_by",
+    CREATE_AT = "create_at",
+    CHANGE_BY = "change_by",
+    CHANGE_AT = "change_at",
+    DATA_FLAG = "data_flag",
+    TOKEN = "token",
+    CONDITION = "condition",
+    DATA = "data",
+    SELECT_ALL = "select_all"
+}
+export declare enum ACTIVE_FLAG {
+    TRUE = "true",
+    FALSE = "false"
+}
+export declare enum DATA_FLAG {
+    NORMAL = 0,
+    IMPORT = 1
+}
+export declare enum RESULT_STATUS {
+    ERROR = "ERROR",
+    OK = "OK",
+    INFO = "INFO",
+    EXCEPTION = "EXCEPTION"
+}
+export declare enum DATE_FORMAT {
+    YYYYMMDD_1 = "yyyy/MM/dd",
+    YYYYMMDD_2 = "yyyy-MM-dd",
+    DDMMYYYY_1 = "dd/MM/yyyy",
+    DDMMYYYY_2 = "dd-MM-yyyy",
+    MMDDYYYY_1 = "mmDDyyyy",
+    MMDDYYYY_2 = "mm-DD-yyyy"
+}
+export declare enum NUMBER_FOMART {
+}
+export declare enum SYMBOLS {
+    COMMA = ",",
+    DOT = ".",
+    EQUAL = "=",
+    PERCENTAGE = "%",
+    QUOTE_SINGLE = "'",
+    QUOTE_DOUBLE = "\""
+}
